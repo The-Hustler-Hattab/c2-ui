7da13166ae3618b0547ea1aa84b6415d282c8c58
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-11-19 21:25:41 -0500

    changed manager websocket behavior

--- src/app/services/websocket-service.service.ts
+++ src/app/services/websocket-service.service.ts
@@ -1,10 +1,11 @@
 import { Inject, Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
-import { Client, IStompSocket, Message } from '@stomp/stompjs';
-import * as SockJS from 'sockjs-client';
+
+
+
 
 
 @Injectable({
@@ -17,17 +18,18 @@ export class WebsocketServiceService {
 
   private messageSubject = new Subject<MessageEvent>();
 
+
   constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth){}
   
   
   connect() {
-    const token = 'Bearer '+ this.oktaAuth.getAccessToken()
+    const token = this.oktaAuth.getAccessToken()
    
     
     if (!this.socket && token) {
       console.log("[+] Connecting to web socket");
       
-      this.socket = new WebSocket(this.wsUrl);
+      this.socket = new WebSocket(this.wsUrl+'?auth='+token);
 
 
 
@@ -55,59 +57,36 @@ export class WebsocketServiceService {
     return this.messageSubject.asObservable();
   }
 
-  // private stompClient: Client | null = null;
-  // private wsUrl = environment.wssurl;
 
-  // private messageSubject = new Subject<Message>();
 
-  // constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {}
 
-  // connect() {
-  //   const token = 'Bearer ' + this.oktaAuth.getAccessToken();
 
-  //   if (!this.stompClient && token) {
-  //     console.log('[+] Connecting to WebSocket');
 
-  //     // Create a SockJS WebSocket connection
-  //     const socket = new SockJS(this.wsUrl);
-  //     this.stompClient = new Client();
 
-  //     // Add the authentication token as a header
-  //     const headers = { Authorization: token };
 
-  //     // Connect to the WebSocket using Stomp
-  //     this.stompClient.webSocketFactory =  new SockJS('your_sockjs_url') ;
-  //     this.stompClient.activate();
-  //     this.stompClient.onConnect = () => {
-  //       // Subscribe to your desired destination with custom headers
-  //       this.stompClient.subscribe(
-  //         '/topic/example',
-  //         (message: Message) => {
-  //           this.messageSubject.next(message);
-  //         },
-  //         { headers }
-  //       );
-  //     };
-  //   }
+
+
+  // private stompClient: any;
+
+  // private token: any;
+
+
+
+  // initializeWebSocketConnection() {
+  //   this.token = { 'Authorization': 'Bearer ' + this.oktaAuth.getAccessToken() };
+
+  //   const socket = new SockJS('');
+  //   this.stompClient = Stomp.over(socket);
+
+  //   this.stompClient.connect(this.token, (frame: any) => {
+  //     console.log('Connected: ' + frame);
+
+  //     // Subscribe to a destination
+  //     this.stompClient.subscribe('/topic/messages', (message: any) => {
+  //       console.log('Received message: ' + message.body);
+  //       // Handle the received message
+  //     });
+  //   });
   // }
-
-  // disconnect() {
-  //   if (this.stompClient) {
-  //     this.stompClient.deactivate();
-  //     this.stompClient = null;
-  //   }
-  // }
-
-  // sendMessage(destination: string, message: string) {
-  //   if (this.stompClient && this.stompClient.connected) {
-  //     // Send a message with custom headers
-  //     this.stompClient.publish({ destination, body: message }, { headers: { Authorization: token } });
-  //   }
-  // }
-
-  // listen(): Observable<Message> {
-  //   return this.messageSubject.asObservable();
-  // }
-
 
 }
