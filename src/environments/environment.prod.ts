969271947036f7dafb58197a461aff781baf74c5
Mohammed Hattab <mohammedhattab97@gmail.com>
2024-03-04 21:02:10 -0500

    migrate to cloud run

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
 export const environment = {
   production: true,
-  rooturl : 'https://c2-server.mtattab.com',
-  wssurl: 'wss://c2-server.mtattab.com/reverseShellManager',
+  // rooturl : 'https://c2-server.mtattab.com',
+  // wssurl: 'wss://c2-server.mtattab.com/reverseShellManager',
   cSharpAPI: 'https://file7exfilterate.azurewebsites.net',
 
-
+  rooturl : 'https://c2-server-tvan66p23a-uc.a.run.app',
+  wssurl: 'wss://c2-server-tvan66p23a-uc.a.run.app/reverseShellManager'
   // cSharpAPI : 'http://localhost:7109',
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   // wssurl: 'ws://127.0.0.1:8070/reverseShellManager'
   // cSharpAPI : 'http://localhost:7109',
 
-  rooturl : 'https://c2-server.mtattab.com',
+  rooturl : 'https://c2-server-tvan66p23a-uc.a.run.app',
   cSharpAPI: 'https://file7exfilterate.azurewebsites.net',
-  wssurl: 'wss://c2-server.mtattab.com/reverseShellManager'
+  wssurl: 'wss://c2-server-tvan66p23a-uc.a.run.app/reverseShellManager'
 
 };
 
